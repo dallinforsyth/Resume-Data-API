e59511e4b0ec3c8b4479ba5eed309b34e7153d15
Madeline Vogel <madmvogel@gmail.com>
2023-03-15 13:38:51 -0500

    add experience model with seeds for initial data

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,5 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the bin/rails db:seed command (or created alongside the database with db:setup).
-#
-# Examples:
-#
-#   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
-#   Character.create(name: "Luke", movie: movies.first)
+Experience.create!(start_date: "01/23/12", end_date: "04/13/16", job_title: "Taxi Driver", company_name: "Relaxi-Cab", details: "Drove a taxicab for mobile massages")
+
+Experience.create!(start_date: "05/04/17", end_date: "12/12/19", job_title: "Buyer", company_name: "Ralph Lauren", details: "Bought clothing for Raplh Lauren")
+
+Experience.create!(start_date: "08/25/22", end_date: "present", job_title: "Paleontologist", company_name: "A University", details: "do dino thangs and teach")
