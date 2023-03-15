2403afd0b112d4a9b8f17000f73893929cbd08c8
alexrcenteno <alex.centeno1991@gmail.com>
2023-03-15 14:15:00 -0500

    Add create action

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -3,4 +3,21 @@ class StudentsController < ApplicationController
     @students = Student.all
     render :index
   end
+
+  def create
+    @student = Student.create(
+      first_name: params[:first_name],
+      last_name: params[:last_name],
+      email: params[:email],
+      phone_number: params[:phone_number],
+      short_bio: params[:short_bio],
+      linkedin_url: params[:linkedin_url],
+      twitter_handle: params[:twitter_handle],
+      personal_blog_website_url: params[:personal_blog_website_url],
+      online_resume_url: params[:online_resume_url],
+      github_url: params[:github_url],
+      password_digest: params[:password_digest],
+    )
+    render :show
+  end
 end
--- test/controllers/students_controller_test.rb
+++ test/controllers/students_controller_test.rb
@@ -8,4 +8,11 @@ class StudentsControllerTest < ActionDispatch::IntegrationTest
     data = JSON.parse(response.body)
     assert_equal Student.count, data.length
   end
+
+  test "create" do
+    assert_difference "Student.count", 1 do
+      post "/students.json", params: { first_name: "Michael", last_name: "Scott", email: "Michaelscott@dundermifflin.com", phone_number: "888-555-5556", short_bio: "I sell the best paper at the best paper company in the world", linkedin_url: "https://www.linkedin/in/michaelscott", twitter_handle: "https://www.twitter.com/michaelscott", personal_blog_website_url: "test.com", online_resume_url: "test.com", github_url: "https://github.com/michaelscott", photo: "test.jpg", password_digest: "[FILTERED]" }
+      assert_response 200
+    end
+  end
 end
