cef4791e4b1a7e3b35f585ef8ebb53d6281a93e6
Madeline Vogel <madmvogel@gmail.com>
2023-03-16 12:27:15 -0500

    add skills model with seeds for initial data

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_15_175209) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_16_172111) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -20,6 +20,12 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_15_175209) do
     t.string "job_title"
     t.string "company_name"
     t.text "details"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "skills", force: :cascade do |t|
+    t.string "name"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
--- db/seeds.rb
+++ db/seeds.rb
@@ -3,3 +3,11 @@ Experience.create!(start_date: "01/23/12", end_date: "04/13/16", job_title: "Tax
 Experience.create!(start_date: "05/04/17", end_date: "12/12/19", job_title: "Buyer", company_name: "Ralph Lauren", details: "Bought clothing for Raplh Lauren")
 
 Experience.create!(start_date: "08/25/22", end_date: "present", job_title: "Paleontologist", company_name: "A University", details: "do dino thangs and teach")
+
+Skill.create!(name: "HTML")
+
+Skill.create!(name: "CSS")
+
+Skill.create!(name: "Ruby")
+
+Skill.create!(name: "JavaScript")
