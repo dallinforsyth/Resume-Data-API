342089a782d18b39d16dc5161f2cc48a01054bae
dallinforsyth <dallfor192@gmail.com>
2023-03-16 10:30:08 -0700

    created all routes

--- config/routes.rb
+++ config/routes.rb
@@ -13,4 +13,8 @@ Rails.application.routes.draw do
   delete "/experiences/:id" => "experiences#destroy"
 
   get "/capstones" => "capstones#index"
+  get "/capstones/:id" => "capstones#show"
+  post "/capstones" => "capstones#create"
+  patch "/capstones/:id" => "capstones#update"
+  delete "/capstones/:id" => "capstones#destroy"
 end
