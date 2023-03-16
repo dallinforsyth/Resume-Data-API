478a0105a7667e98392756a022da47600d6e6fdb
dallinforsyth <dallfor192@gmail.com>
2023-03-16 10:33:28 -0700

    added create method

--- app/controllers/capstones_controller.rb
+++ app/controllers/capstones_controller.rb
@@ -8,4 +8,14 @@ class CapstonesController < ApplicationController
     @capstone = Capstone.find_by(id: params[:id])
     render :show
   end
+
+  def create
+    @capstone = Capstone.create(
+      name: params[:name],
+      description: params[:description],
+      url: params[:url],
+      screenshot: params[:screenshot],
+    )
+    render :show
+  end
 end
