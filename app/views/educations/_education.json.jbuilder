fcce3e4a61aebca7c069563d60fb20abe9c89b84
dallinforsyth <dallfor192@gmail.com>
2023-03-16 09:22:19 -0700

    added cors file, updated destory message, and tests.

--- app/views/educations/_education.json.jbuilder
+++ app/views/educations/_education.json.jbuilder
@@ -1,2 +1,8 @@
-json.extract! education, :id, :created_at, :updated_at
-json.url education_url(education, format: :json)
+json.id education.id
+json.start_date education.start_date
+json.end_date education.end_date
+json.degree education.degree
+json.university_name education.university_name
+json.details education.details
+json.created_at education.created_at
+json.updated_at education.updated_at
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  get "/educations" => "educations#index"
+  get "/educations/:id" => "educations#show"
+  post "/educations" => "educations#create"
+  patch "/educations/:id" => "educations#update"
+  delete "/educations/:id" => "educations#destroy"
 end
