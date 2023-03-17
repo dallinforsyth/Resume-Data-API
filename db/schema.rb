8a855772c25578fd963dbcca9a5ddb731a9de61f
alexrcenteno <alex.centeno1991@gmail.com>
2023-03-17 11:15:20 -0500

    added view/json

--- db/schema.rb
+++ db/schema.rb
@@ -10,23 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_15_183213) do
-  # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
-
-  create_table "students", force: :cascade do |t|
-    t.string "first_name"
-    t.string "last_name"
-    t.string "email"
-    t.string "phone_number"
-    t.string "short_bio"
-    t.string "linkedin_url"
-    t.string "twitter_handle"
-    t.string "personal_blog_website_url"
-    t.string "online_resume_url"
-    t.string "github_url"
-    t.string "photo"
-    t.string "password_digest"
 ActiveRecord::Schema[7.0].define(version: 2023_03_16_190545) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -68,6 +51,23 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_16_190545) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "student_id"
+  end
+
+  create_table "students", force: :cascade do |t|
+    t.string "first_name"
+    t.string "last_name"
+    t.string "email"
+    t.string "phone_number"
+    t.string "short_bio"
+    t.string "linkedin_url"
+    t.string "twitter_handle"
+    t.string "personal_blog_website_url"
+    t.string "online_resume_url"
+    t.string "github_url"
+    t.string "photo"
+    t.string "password_digest"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
 end
